5ad0e273eb3ed824bd6d80d9fff85144fa462456
Clement Farabet <clement.farabet@gmail.com>
2011-07-08 15:38:07 -0400

    Fixed ConfusionMatrix:add()

--- ConfusionMatrix.lua
+++ ConfusionMatrix.lua
@@ -4,6 +4,7 @@ local ConfusionMatrix = torch.class('nn.ConfusionMatrix')
 function ConfusionMatrix:__init(nclasses, classes)
    self.mat = lab.zeros(nclasses,nclasses)
    self.valids = lab.zeros(nclasses)
+   self.nclasses = nclasses
    self.totalValid = 0
    self.averageValid = 0
    self.classes = classes
@@ -15,8 +16,10 @@ function ConfusionMatrix:add(prediction, target)
       self.mat[target][prediction] = self.mat[target][prediction] + 1
    else
       -- comparing vectors
-      local _,prediction = lab.max(prediction)
-      local _,target = lab.max(target)
+      local prediction_1d = torch.Tensor(prediction):resize(self.nclasses)
+      local target_1d = torch.Tensor(target):resize(self.nclasses)
+      local _,prediction = lab.max(prediction_1d)
+      local _,target = lab.max(target_1d)
       self.mat[target[1]][prediction[1]] = self.mat[target[1]][prediction[1]] + 1
    end
 end
@@ -30,14 +33,14 @@ end
 
 function ConfusionMatrix:updateValids()
    local total = 0
-   for t = 1,self.mat:size(1) do
+   for t = 1,self.nclasses do
       self.valids[t] = self.mat[t][t] / self.mat:select(1,t):sum()
       total = total + self.mat[t][t]
    end
    self.totalValid = total / self.mat:sum()
    self.averageValid = 0
    local nvalids = 0
-   for t = 1,self.mat:size(1) do
+   for t = 1,self.nclasses do
       if not sys.isNaN(self.valids[t]) then
          self.averageValid = self.averageValid + self.valids[t]
          nvalids = nvalids + 1
@@ -49,7 +52,7 @@ end
 function ConfusionMatrix:__tostring__()
    self:updateValids()
    local str = 'ConfusionMatrix:\n'
-   local nclasses = self.mat:size(1)
+   local nclasses = self.nclasses
    str = str .. '['
    for t = 1,nclasses do
       local pclass = self.valids[t] * 100
