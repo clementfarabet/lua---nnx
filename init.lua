597c2d99706268066d7907df34dc446d656d4c00
Clement Farabet <clement.farabet@gmail.com>
2011-07-26 20:29:55 -0400

    Added Marco and Ben in copyrights.

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------
 --
--- Copyright (c) 2011 Clement Farabet, Koray Kavukcuoglu
+-- Copyright (c) 2011 Clement Farabet, Marco Scoffier, Koray Kavukcuoglu
 -- 
 -- Permission is hereby granted, free of charge, to any person obtaining
 -- a copy of this software and associated documentation files (the
