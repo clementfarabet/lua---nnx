ce4effdc67d16422ef45b2deeaab485c98b0c4ce
Clement Farabet <clement.farabet@gmail.com>
2011-07-28 16:49:27 -0400

    Using Koray's flag to activate openmp

--- OmpModule.lua
+++ OmpModule.lua
@@ -2,10 +2,18 @@ local OmpModule,parent = torch.class('nn.OmpModule','nn.Module')
 
 function OmpModule:__init()
    parent.__init(self)
-   self.threads = 1
+   if openmp then
+      self.threads = openmp.getNumThreads()
+   else
+      self.threads = 1
+   end
 end
 
 function OmpModule:read(file)
    parent.read(self, file)
-   self.threads = 1
+   if openmp then
+      self.threads = openmp.getNumThreads()
+   else
+      self.threads = 1
+   end
 end
