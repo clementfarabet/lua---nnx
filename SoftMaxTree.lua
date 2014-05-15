91b74dae2b06ed39f87a6f9e699904b1ae3c2c7c
nicholas-leonard <nick@nikopia.org>
2014-05-15 15:28:35 -0400

    SoftMaxTree.lua

---
+++ SoftMaxTree.lua
@@ -0,0 +1,114 @@
+local SoftMaxTree, parent = torch.class('nn.SoftMaxTree', 'nn.Module')
+
+-- TODO: a shareClone method to make speedier clones
+-- differ setup
+function SoftMaxTree:__init(inputSize, hierarchy, rootId, verbose)
+   parent.__init(self)
+   self.rootId = rootId or 0
+   self.inputSize = inputSize
+   assert(type(hierarchy) == 'table', "Expecting table at arg 2")
+   -- get the total amount of children (non-root nodes)
+   local nChildNode = 0
+   local nParentNode = 0
+   local maxNodeId = -99999999
+   local minNodeId = 999999999
+   local maxParentId = -99999999
+   local maxChildId = -9999999
+   local parentIds = {}
+   for parentId, children in pairs(hierarchy) do
+      assert(children:dim() == 1, "Expecting table of 1D tensors at arg 2")
+      nChildNode = nChildNode + children:size(1)
+      nParentNode = nParentNode + 1
+      maxParentId = math.max(parentId, maxParentId)
+      local maxChildrenId = children:max()
+      maxChildId = math.max(maxChildrenId, maxChildId)
+      maxNodeId = math.max(parentId, maxNodeId, maxChildrenId)
+      minNodeId = math.max(parentId, minNodeId, children:min())
+      table.insert(parentIds, parentId)
+   end
+   assert(minNodeId >= 0, "nodeIds must must be non-negative")
+   if verbose then
+      print("Hierachy has :")
+      print(nParentNode.." parent nodes")
+      print(nChildNode.." child nodes")
+      print((nChildNode - nParentNode).." leaf nodes")
+      print("node index will contain "..maxNodeId.." slots")
+      if maxNodeId ~= (nChildNode + 1) then
+         print("Warning: Hierarchy has more nodes than Ids")
+         print("Consider making your nodeIds a contiguous sequence ")
+         print("in order to waste less memory on indexes.")
+      end
+   end
+   self.nChildNode = nChildNode
+   self.nParentNode = nParentNode
+   self.minNodeId = minNodeId
+   self.maxNodeId = maxNodeId
+   self.maxParentId = maxParentId
+   self.maxChildId = maxChildId
+   -- initialize weights and biases
+   self.weight = torch.Tensor(self.nChildNode, self.inputSize)
+   self.bias = torch.Tensor(self.nChildNode)
+   self.gradWeight = torch.Tensor(self.nChildNode, self.inputSize)
+   self.gradBias = torch.Tensor(self.nChildNode)
+   
+   -- contains all childIds
+   self.childIds = torch.IntTensor(self.nChildNode)
+   -- contains all parentIds
+   self.parentIds = torch.IntTensor(parentIds)
+   
+   -- index of children by parentId
+   self.parentChildren = torch.IntTensor(self.maxParentId, 2):fill(-1)
+   local start = 1
+   for parentId, children in pairs(hierarchy) do
+      local node = self.parentChildren:select(1, parentId)
+      node[1] = start
+      local nChildren = children:size()
+      node[2] = nChildren
+      self.childIds:narrow(1, start, nChildren):copy(children)
+      start = start + nChildren
+   end
+   
+   -- index of parent by childId
+   self.childParent = torch.IntTensor(self.maxChildId):fill(-1)
+   for parendIdx=1,self.parentIds:size(1) do
+      local parentId = self.parentIds[parentIdx]
+      local node = self.parentChildren:select(1, parentId)
+      local start = node[1]
+      local nChildren = node[2]
+      local children = self.childIds:narrow(1, start, nChildren)
+      for childIdx=1,children:size(1) do
+         local childId = children[childIdx]
+         self.childParent[childId] = parentId
+      end
+   end
+   
+   self:reset()
+end
+
+function SoftMaxTree:reset(stdv)
+   if stdv then
+      stdv = stdv * math.sqrt(3)
+   else
+      stdv = 1/math.sqrt(self.nChildNode*self.inputSize)
+   end
+   self.weight:uniform(-stdv, stdv)
+   self.bias:uniform(-stdv, stdv)
+end
+
+function SoftMaxTree:updateOutput(input)
+   return input.nn.SoftMaxTree_updateOutput(self, input)
+end
+
+function SoftMaxTree:updateGradInput(input, gradOutput)
+   if self.gradInput then
+      return input.nn.SoftMaxTree_updateGradInput(self, input, gradOutput)
+   end
+end
+
+function SoftMaxTree:accGradParameters(input, gradOutput, scale)
+   scale = scale or 1
+   input.nn.SoftMaxTree_accGradParameters(self, input, gradOutput, scale)
+end
+
+-- we do not need to accumulate parameters when sharing
+SoftMaxTree.sharedAccUpdateGradParameters = SoftMaxTree.accUpdateGradParameters
